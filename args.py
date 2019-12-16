77f90c3c7546611230b7990f909b90c81165455c
ckfChao <tonychao3699@gmail.com>
2019-12-16 13:55:10 +0800

    update args.py

--- args.py
+++ args.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--start-date", required=True, help="Crawl Start date")
-    parser.add_argument("--end-date", required=True)
+    parser.add_argument("--start-date", required=True, help="Start date")
+    parser.add_argument("--end-date", required=True, help="Enddate")
+    parser.add_argument("--output", default="output.csv", help="file name")
     args = parser.parse_args()
     args.start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
     args.end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
