d435a67777ae93416fa2021a85d22bd24517a155
ckfChao <tonychao3699@gmail.com>
2019-12-18 18:28:25 +0800

    update args function

--- args.py
+++ args.py
@@ -1,10 +1,12 @@
 import argparse
 from datetime import datetime
 
-
 def get_args():
-    # TODO: Add --start-date, --end-date and --output arguments
-    #       Convert the two dates to datetime objects
     parser = argparse.ArgumentParser()
+    parser.add_argument("--start-date", required=True, help="Start date")
+    parser.add_argument("--end-date", required=True, help="Enddate")
+    parser.add_argument("--output", default="output.csv", help="file name")
     args = parser.parse_args()
+    args.start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
+    args.end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
     return args
