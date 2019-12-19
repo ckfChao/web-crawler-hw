010ac5c4a82a6098f3f4efc1d8e24573478ee562
ckfChao <tonychao3699@gmail.com>
2019-12-19 09:39:50 +0800

    add main function

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ from args import get_args
 if __name__ == '__main__':
     args = get_args()
     crawler = Crawler()
-    content = crawler.crawl(args.start_date, args.end_date)
+    contents = crawler.crawl(args.start_date, args.end_date)
     # TODO: write content to file according to spec
+    with open(args.output, 'w', encoding='utf-8') as f:
+        for date, title, content in contents:
+            out_str = f'{str(date)},"{title}", "{content}"\n'
+            f.write(out_str)
